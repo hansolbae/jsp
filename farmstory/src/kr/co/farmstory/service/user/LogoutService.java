71b1ce77fb1839f579e06c3d62ee4f588fd5f20d
bigdata <bigdata@user-PC>
2019-06-11 11:50:08 +0900

    2019/06/11 로그아웃 구현

--- farmstory/src/kr/co/farmstory/service/user/LogoutService.java
+++ farmstory/src/kr/co/farmstory/service/user/LogoutService.java
@@ -2,6 +2,7 @@ package kr.co.farmstory.service.user;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kr.co.farmstory.controller.CommonService;
 
@@ -9,7 +10,11 @@ public class LogoutService implements CommonService {
 
 	@Override
 	public String requestProc(HttpServletRequest req, HttpServletResponse resp) {
-		return "/index.jsp";
+		
+		HttpSession sess = req.getSession();
+		sess.invalidate();
+		
+		return "redirect:/farmstory/index.do";
 	}
 	
 }
